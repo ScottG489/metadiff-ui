2fcd27b3c82a4495dbd338aeaed57290b3676414
Scott Giminiani <scottg489@gmail.com>
2019-01-11 08:34:45 -0800

    Make files public when deploying on on s3

--- util/build.sh
+++ util/build.sh
@@ -11,4 +11,4 @@ git clone git@github.com:ScottG489/diff-info-service-ui.git
 cd diff-info-service-ui
 npm install
 npm run build
-aws s3 sync build/ s3://diff-info-service-ui
+aws s3 sync build/ s3://diff-info-service-ui --acl public-read
