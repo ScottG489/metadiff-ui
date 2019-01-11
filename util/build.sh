9752832084ef07f3b1fdf0120c762ce27ccadd8f
Scott Giminiani <scottg489@gmail.com>
2019-01-10 20:42:00 -0800

    Add self contained build and deploy within docker container
    - Dockerfile produces image which contains all dependencies required to
      build and deploy (git, npm, aws, config files, etc)
    - Build image takes two arguments which are credentials for cloning the
      code repo and aws for deploying

---
+++ util/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ID_RSA_CONTENTS=$1
+AWS_CREDENTIALS_CONTENTS=$2
+
+printf -- "$ID_RSA_CONTENTS" > /root/.ssh/id_rsa
+printf -- "$AWS_CREDENTIALS_CONTENTS" > /root/.aws/credentials
+
+chmod 400 /root/.ssh/id_rsa
+
+git clone git@github.com:ScottG489/diff-info-service-ui.git
+cd diff-info-service-ui
+npm install
+npm run build
+aws s3 sync build/ s3://diff-info-service-ui
