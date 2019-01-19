977c371fe01af18d931fe696eef8c9f5ef2987ac
Scott Giminiani <scottg489@gmail.com>
2019-01-19 01:33:48 -0800

    Create bucket and set its website properties as part of deployment

--- util/build.sh
+++ util/build.sh
@@ -7,4 +7,6 @@ git clone git@github.com:ScottG489/diff-info-service-ui.git
 cd diff-info-service-ui
 npm install
 npm run build
+aws s3 mb s3://diff-info-service-ui
+aws s3 website s3://diff-info-service-ui --index-document index.html
 aws s3 sync build/ s3://diff-info-service-ui --acl public-read
