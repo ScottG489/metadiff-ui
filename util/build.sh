5ffcc6464b8064f13bed2684753885a039ae63ab
Scott Giminiani <scottg489@gmail.com>
2019-01-19 01:40:47 -0800

    Only create bucket if it doesn't exist

--- util/build.sh
+++ util/build.sh
@@ -7,6 +7,9 @@ git clone git@github.com:ScottG489/diff-info-service-ui.git
 cd diff-info-service-ui
 npm install
 npm run build
-aws s3 mb s3://diff-info-service-ui
+
+# Only need to run first time. Is there a more elegant way to do this?
+aws s3 mb s3://diff-info-service-ui || true
 aws s3 website s3://diff-info-service-ui --index-document index.html
+
 aws s3 sync build/ s3://diff-info-service-ui --acl public-read
