db8a6acdb09665141b9d8c2e1e52c13d92fcf3af
Scott Giminiani <scottg489@gmail.com>
2019-04-10 17:19:34 -0700

    Accept credentials for build and deploy as a JSON string
    Right now only credentials cannot be hardcoded so we need to pass them
    in as inputs to the build. Note that the values of the credentials are
    base64 encoded. I don't like this and would rather have something more
    susinctly just JSON escape them but couldn't find a simple way to do
    this in bash.
    
    - Build image now requires jq

--- util/build.sh
+++ util/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+ID_RSA_CONTENTS=$(echo -n $1 | jq -r .ID_RSA | base64 --decode)
+AWS_CREDENTIALS_CONTENTS=$(echo -n $1 | jq -r .AWS_CREDENTIALS | base64 --decode)
+
+printf -- "$ID_RSA_CONTENTS" > /root/.ssh/id_rsa
+printf -- "$AWS_CREDENTIALS_CONTENTS" > /root/.aws/credentials
+
 chmod 400 /root/.ssh/id_rsa
 
 git clone git@github.com:ScottG489/diff-info-service-ui.git
