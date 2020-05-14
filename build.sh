87b5fe92f4780d4e966f28282aefd63ac31e743e
Scott G <ScottG489@Gmail.com>
2020-05-14 00:49:18 -0700

    Change URL for build server

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 'https://diff-data.com/build?image=scottg489/diff-info-service-ui-build:latest'
+curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 'simple-ci.com/build?image=scottg489/diff-info-service-ui-build:latest'
