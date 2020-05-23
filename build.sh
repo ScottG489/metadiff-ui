bc5ba1d4cbb6a46a3419080de36fb503c8bc2d59
Scott G <ScottG489@Gmail.com>
2020-05-23 12:06:55 -0700

    Add protocol to build server URL for clarity

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 'simple-ci.com:8080/build?image=scottg489/diff-info-service-ui-build:latest'
+curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 'http://simple-ci.com:8080/build?image=scottg489/diff-info-service-ui-build:latest'
