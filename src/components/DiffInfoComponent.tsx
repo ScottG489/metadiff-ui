4f1c4fc3fdd570e75cb6a94819387422fe3104e4
Scott Giminiani <scottg489@gmail.com>
2020-05-23 01:40:19 -0700

    Reformat code

--- src/components/DiffInfoComponent.tsx
+++ src/components/DiffInfoComponent.tsx
@@ -6,84 +6,84 @@ export interface DiffInfoProps {
 }
 
 const DiffInfoComponent: FC<DiffInfoProps> = ({diffInfo}) => {
-  return (
-    <div className="row">
-      <div className="col">
-        <table className="table table-sm table-striped">
-          <tbody>
-          <tr>
-            <th>To file:</th>
-            <th>{diffInfo.toFile}</th>
-          </tr>
-          <tr>
-            <th>From file:</th>
-            <th>{diffInfo.fromFile}</th>
-          </tr>
-          <tr>
-            <th>File status:</th>
-            <th>{diffInfo.fileStatus}</th>
-          </tr>
-          <tr>
-            <th>Is Added File:</th>
-            <th>{convertToString(diffInfo.addedFile)}</th>
-          </tr>
-          <tr>
-            <th>Is Modified File:</th>
-            <th>{convertToString(diffInfo.modifiedFile)}</th>
-          </tr>
-          <tr>
-            <th>Is Removed File:</th>
-            <th>{convertToString(diffInfo.removedFile)}</th>
-          </tr>
-          <tr>
-            <th>Is Renamed file:</th>
-            <th>{convertToString(diffInfo.renamed)}</th>
-          </tr>
-          <tr>
-            <th>Is Binary File:</th>
-            <th>{convertToString(diffInfo.binary)}</th>
-          </tr>
-          <tr>
-            <th>Mode:</th>
-            <th>{diffInfo.mode}</th>
-          </tr>
-          <tr>
-            <th>Old Mode:</th>
-            <th>{diffInfo.oldMode}</th>
-          </tr>
-          <tr>
-            <th>Checksum Before:</th>
-            <th>{diffInfo.checksumBefore}</th>
-          </tr>
-          <tr>
-            <th>Checksum After:</th>
-            <th>{diffInfo.checksumAfter}</th>
-          </tr>
-          <tr>
-            <th>Is Copied File:</th>
-            <th>{convertToString(diffInfo.copied)}</th>
-          </tr>
-          <tr>
-            <th>Similarity Index:</th>
-            <th>{diffInfo.similarityIndex}</th>
-          </tr>
-          <tr>
-            <th>Disimilarity Index:</th>
-            <th>{diffInfo.disimilarityIndex}</th>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className="col">
-        <h5>Raw Diff:</h5>
-        <textarea className="form-control" rows={18} value={diffInfo.rawDiff} readOnly/>
-      </div>
-    </div>
-  )
+    return (
+        <div className="row">
+            <div className="col">
+                <table className="table table-sm table-striped">
+                    <tbody>
+                    <tr>
+                        <th>To file:</th>
+                        <th>{diffInfo.toFile}</th>
+                    </tr>
+                    <tr>
+                        <th>From file:</th>
+                        <th>{diffInfo.fromFile}</th>
+                    </tr>
+                    <tr>
+                        <th>File status:</th>
+                        <th>{diffInfo.fileStatus}</th>
+                    </tr>
+                    <tr>
+                        <th>Is Added File:</th>
+                        <th>{convertToString(diffInfo.addedFile)}</th>
+                    </tr>
+                    <tr>
+                        <th>Is Modified File:</th>
+                        <th>{convertToString(diffInfo.modifiedFile)}</th>
+                    </tr>
+                    <tr>
+                        <th>Is Removed File:</th>
+                        <th>{convertToString(diffInfo.removedFile)}</th>
+                    </tr>
+                    <tr>
+                        <th>Is Renamed file:</th>
+                        <th>{convertToString(diffInfo.renamed)}</th>
+                    </tr>
+                    <tr>
+                        <th>Is Binary File:</th>
+                        <th>{convertToString(diffInfo.binary)}</th>
+                    </tr>
+                    <tr>
+                        <th>Mode:</th>
+                        <th>{diffInfo.mode}</th>
+                    </tr>
+                    <tr>
+                        <th>Old Mode:</th>
+                        <th>{diffInfo.oldMode}</th>
+                    </tr>
+                    <tr>
+                        <th>Checksum Before:</th>
+                        <th>{diffInfo.checksumBefore}</th>
+                    </tr>
+                    <tr>
+                        <th>Checksum After:</th>
+                        <th>{diffInfo.checksumAfter}</th>
+                    </tr>
+                    <tr>
+                        <th>Is Copied File:</th>
+                        <th>{convertToString(diffInfo.copied)}</th>
+                    </tr>
+                    <tr>
+                        <th>Similarity Index:</th>
+                        <th>{diffInfo.similarityIndex}</th>
+                    </tr>
+                    <tr>
+                        <th>Disimilarity Index:</th>
+                        <th>{diffInfo.disimilarityIndex}</th>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className="col">
+                <h5>Raw Diff:</h5>
+                <textarea className="form-control" rows={18} value={diffInfo.rawDiff} readOnly/>
+            </div>
+        </div>
+    )
 }
 
 const convertToString = (obj: any) => {
-  if (obj !== null && obj !== undefined) return obj.toString()
+    if (obj !== null && obj !== undefined) return obj.toString()
 };
 
 export default DiffInfoComponent
--- src/components/DiffInputComponent.tsx
+++ src/components/DiffInputComponent.tsx
@@ -8,46 +8,44 @@ interface DiffInputProps {
 }
 
 let DiffInputComponent: FC<DiffInputProps> = ({diffInput, refresh}) => {
-  const [textArea, setTextArea] = useState(diffInput);
+    const [textArea, setTextArea] = useState(diffInput);
 
-  return (
-    <form onSubmit={
-      async (event: React.FormEvent) => {
-          let diffInfo = await submitDiffInputText(event, textArea);
-          refresh(diffInfo)
-      }
-    }>
-      <div className="form-group">
+    return (
+        <form onSubmit={
+            async (event: React.FormEvent) => {
+                let diffInfo = await submitDiffInputText(event, textArea);
+                refresh(diffInfo)
+            }
+        }>
+            <div className="form-group">
         <textarea
-          className="form-control"
-          rows={10}
-          value={textArea}
+            className="form-control"
+            rows={10}
+            value={textArea}
 
-          onChange={(event) => {
-              setTextArea(event.target.value);
-          }}
+            onChange={(event) => {
+                setTextArea(event.target.value);
+            }}
         />
-      </div>
-      <div className="form-group">
-        <input className="form-control" type="submit" value="Submit"/>
-      </div>
-    </form>
-  )
+            </div>
+            <div className="form-group">
+                <input className="form-control" type="submit" value="Submit"/>
+            </div>
+        </form>
+    )
 };
 
 async function submitDiffInputText(event: React.FormEvent, input: string): Promise<DiffInfo> {
-  event.preventDefault();
-  let foo = await fetch('http://simple-ci.com:8080/build?image=scottg489/diff-info:latest&pull=\n' +
-      'false', {
-    method: 'POST',
-    body: input
-  })
-    .then(async response => {
-      return response.json()
+    event.preventDefault();
+    return await fetch('http://simple-ci.com:8080/build?image=scottg489/diff-info:latest&pull=\n' +
+        'false', {
+        method: 'POST',
+        body: input
     })
-    .catch(reason => console.log("Failure reason: " + reason));
-
-  return foo;
+        .then(async response => {
+            return response.json()
+        })
+        .catch(reason => console.log("Failure reason: " + reason));
 }
 
 export default DiffInputComponent
