992b4a111e04df384555403b4f97e1d966c579af
Scott G <scottg489@gmail.com>
2020-06-27 02:54:41 -0700

    Use wget instead of curl to start builds

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-curl -v -s -f --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 'http://simple-ci.com/build?image=scottg489/diff-info-service-ui-build:latest'
+wget --content-on-error -nv -O - --post-data '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 'http://simple-ci.com/build?image=scottg489/diff-info-service-ui-build:latest'
