af8f303420740ceb95ae7c1382258858349c3a6f
Scott G <scottg489@gmail.com>
2020-06-26 20:02:29 -0700

    Use port 80 for build server
    The port for the build server was changed from 8080 to 80

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-curl -v -s -f --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 'http://simple-ci.com:8080/build?image=scottg489/diff-info-service-ui-build:latest'
+curl -v -s -f --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 'http://simple-ci.com/build?image=scottg489/diff-info-service-ui-build:latest'
