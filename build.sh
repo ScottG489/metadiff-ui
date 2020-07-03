6c4fffe6a46e5d6e83bd16c145a960aa84b34414
Scott G <scottg489@gmail.com>
2020-07-02 21:59:09 -0700

    Use curl to run build on build server
    This will cause the build to fail when the server returns an error http
    status code

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-wget --content-on-error -nv -O - --post-data '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' 
-
 curl -v -sS -w '%{http_code}' \
   --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' \
   'http://simple-ci.com/build?image=scottg489/diff-info-service-ui-build:latest' \
