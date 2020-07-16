f3edf02a6ebe395afd1b44929a55157799f41748
Scott G <scottg489@gmail.com>
2020-07-16 03:10:38 -0700

    Access bucket info via outputs
    This method is much cleaner and safer than the previous method. This
    requires an explicit output to be defined. It also won't change if the
    structure of the resources, modules, etc. change.

--- infra/tf/modules/diff_data_website_core/outputs.tf
+++ infra/tf/modules/diff_data_website_core/outputs.tf
@@ -1,3 +1,11 @@
+output "bucket" {
+  value = module.helpers_s3_website.bucket_name
+}
+
+output "bucket_website_endpoint" {
+  value = module.helpers_s3_website.website_endpoint
+}
+
 output "r53_zone_name" {
   value = aws_route53_zone.r53_zone.name
 }
---
+++ infra/tf/test-env/outputs.tf
@@ -0,0 +1,7 @@
+output "bucket" {
+  value = module.diff_data_website.bucket
+}
+
+output "bucket_website_endpoint" {
+  value = module.diff_data_website.bucket_website_endpoint
+}
