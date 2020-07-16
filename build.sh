951410b5a45d2da57b8f8c1067d0cb603ad6c28e
Scott G <scottg489@gmail.com>
2020-07-16 03:58:19 -0700

    Update build server URL

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 curl -v -sS -w '%{http_code}' \
   --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' \
-  'http://simple-ci.com/build?image=scottg489/diff-info-service-ui-build:latest' \
+  'http://api.simple-ci.com/build?image=scottg489/diff-info-service-ui-build:latest' \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
