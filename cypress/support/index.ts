65162afc0230c4c7f562db951721260dbd6577ab
Scott Giminiani <scottg489@gmail.com>
2022-04-02 14:23:15 -0400

    Fix formatting

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,9 +1,9 @@
 describe('Once before all tests', () => {
   it('should make a request to warm up the backend endpoint', () => {
-      cy.request({
-          url: Cypress.env('backendUrl'),
-          timeout: 180000, // 3 minutes in ms
-          failOnStatusCode: false
-      })
+    cy.request({
+      url: Cypress.env('backendUrl'),
+      timeout: 180000, // 3 minutes in ms
+      failOnStatusCode: false,
+    })
   })
 })
