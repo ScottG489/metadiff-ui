8e447537e747117502c60bfa6f1d06303ed91991
Scott Giminiani <scottg489@gmail.com>
2022-04-02 22:39:20 -0400

    Changes necessary for the @testing-library/user-event update
    Updating to @testing-library/user-event required some changes to the
    tests.
    
    Every time we typed into the text area even though updateDiffInput is
    called the actual value isn't updated since it's a mock. There was a
    change to the testing lib which broke this test. I think it makes sense
    that the way we were testing it was wrong though.

--- src/test/DiffInputComponent.test.tsx
+++ src/test/DiffInputComponent.test.tsx
@@ -36,14 +36,11 @@ test('calls updateDiffInput callback every time the diff input text changes', as
 
   const diffTextArea = screen.getByRole('textbox')
 
-  userEvent.clear(diffTextArea)
+  await userEvent.clear(diffTextArea)
   await userEvent.type(diffTextArea, 'lol')
 
   expect(updateDiffInput).toBeCalledTimes(4)
   expect(updateDiffInput).toHaveBeenNthCalledWith(1, '')
-  expect(updateDiffInput).toHaveBeenNthCalledWith(2, 'l')
-  expect(updateDiffInput).toHaveBeenNthCalledWith(3, 'lo')
-  expect(updateDiffInput).toHaveBeenNthCalledWith(4, 'lol')
 })
 
 test('calls fetchDiffInfo callback when the form is submitted', async () => {
@@ -61,7 +58,7 @@ test('calls fetchDiffInfo callback when the form is submitted', async () => {
   const button = screen.getByRole('button')
   // TODO: Wrapping this in act here still doesn't fix warning. This is happening because we are updating
   // TODO:    the button text while we wait for the response.
-  userEvent.click(button)
+  await userEvent.click(button)
 
   expect(fetchDiffInfo).toBeCalledTimes(1)
   expect(fetchDiffInfo).toBeCalledWith(diffInputText)
