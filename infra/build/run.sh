b0371e62eda8709238050eaa23d1ec36296107ce
Scott Giminiani <scottg489@gmail.com>
2025-11-09 12:25:00 -0500

    Support different build tasks
    Adds support for specifying a build task when calling build.sh.
    
    Consequences of this are that now we somewhat unfortunately have some
    conditional logic in the build pipeline which I was trying to avoid, but
    I think this is kinda unavoidable. It helps with calling test.sh because
    previously I'd just manually comment out the deploy step which was
    pretty hacky, though it doesn't address the git clone issue.
    
    This change was initiated to support PRs and branches, mainly so we can
    use things like renovate.

--- infra/build/run.sh
+++ infra/build/run.sh
@@ -11,6 +11,7 @@ declare -r _PROJECT_NAME='metadiff-ui'
 declare -r _GIT_REPO='git@github.com:ScottG489/metadiff-ui.git'
 # Used for the domain name but also the s3 bucket (AWS requires them to be the same)
 declare -r _TFSTATE_BUCKET_NAME='tfstate-metadiff.com'
+declare -r _RUN_TASK=$(echo -n $1 | jq -r .RUN_TASK)
 
 git clone $_GIT_REPO
 cd $_PROJECT_NAME
@@ -19,6 +20,7 @@ build_package_application
 
 /home/build-user/build/run-test.sh
 
+if [ "$_RUN_TASK" != "deploy" ]; then exit 0; fi
 tf_backend_init $_TFSTATE_BUCKET_NAME
 
 tf_apply "infra/tf"
