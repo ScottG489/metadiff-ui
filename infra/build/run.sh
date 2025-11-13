04ec8877d0f9656d43a2a1deaab30cd41cb42361
Scott Giminiani <scottg489@gmail.com>
2025-11-12 21:51:05 -0500

    Use a less verbose method to parse build args
    Since we were echo'ing and have set -x it would write out the entire
    json arguments object. While being too verbose, it also contains
    secrets we don't want printed.

--- infra/build/run.sh
+++ infra/build/run.sh
@@ -11,8 +11,8 @@ declare -r _PROJECT_NAME='metadiff-ui'
 declare -r _GIT_REPO='git@github.com:ScottG489/metadiff-ui.git'
 # Used for the domain name but also the s3 bucket (AWS requires them to be the same)
 declare -r _TFSTATE_BUCKET_NAME='tfstate-metadiff.com'
-declare -r _RUN_TASK=$(echo -n $1 | jq -r .RUN_TASK)
-declare -r _GIT_BRANCH=$(echo -n $1 | jq -r .GIT_BRANCH)
+declare -r _RUN_TASK=$(jq -r .RUN_TASK <<< "$1")
+declare -r _GIT_BRANCH=$(jq -r .GIT_BRANCH <<< "$1")
 
 git clone --branch $_GIT_BRANCH $_GIT_REPO
 cd $_PROJECT_NAME
